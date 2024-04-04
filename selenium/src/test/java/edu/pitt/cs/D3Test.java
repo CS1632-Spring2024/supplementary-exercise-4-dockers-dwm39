452e0af0d119ee7372a7b12b76b54efdfed8dee4
dwm39 <dwm39@pitt.edu>
2024-04-04 17:15:42 -0400

    bruh2

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -37,9 +37,6 @@ public class D3Test {
 
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     driver = new ChromeDriver(options);
